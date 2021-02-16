dc4a643d0b89d643f288e14d3f9e0e4025447e69
Semih Celek <cybermoon1999@gmail.com>
2021-02-16 19:16:19 +0300

    add logger morgan

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const logger = require('morgan');
 const userRouter = require('./routes/user-route');
+// const models = require('./models/index')
 
 const app = express();
 app.use(cors());
 
 app.set('view engine', 'pug');
+app.use(logger('dev'));
 app.use(bodyParser.json());
 
 app.use('/user', userRouter);
