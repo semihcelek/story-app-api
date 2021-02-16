6772099876b2b5152ffad56d6417594646cb2c88
Semih Celek <cybermoon1999@gmail.com>
2021-02-16 22:47:40 +0300

    add crud operation routes for user

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,9 +1,46 @@
 const express = require('express');
+// const UserService = require('../services/user-service')
+const { user } = require('../models/index');
 
 const router = express.Router();
 
-router.get('/all', (req, res) => {
-  res.send('<h1>router is working congrats!</h1>');
+router.get('/all/json', async (req, res) => {
+  const people = await user.findAll();
+
+  res.send(people);
+});
+
+router.get('/:id/json', async (req, res) => {
+  const person = await user.findAll({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.send(person);
+});
+
+router.post('/', async (req, res) => {
+  const newUser = await user.create(req.body);
+  res.send(newUser);
+});
+
+router.put('/:id', async (req, res) => {
+  const updateUser = await user.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.render('data', { data: updateUser });
+});
+
+router.delete('/:id', async (req, res) => {
+  console.log(`Im now deleting the ${req.params.id}`);
+  const byeBye = await user.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.send('<alert>User is deleted.</alert>');
 });
 
 module.exports = router;
