61e953e6ae1e70ead83959775c3d921b62a127a7
Semih Celek <cybermoon1999@gmail.com>
2021-02-22 19:05:09 +0300

    add login and register routes

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const logger = require('morgan');
-const userRouter = require('./routes/user-route');
-const storyRouter = require('./routes/story-router');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const logger = require("morgan");
+const userRouter = require("./routes/user-route");
+const storyRouter = require("./routes/story-router");
+const loginRoute = require("./routes/login-router");
+const registerRoute = require("./routes/register-router");
 // const models = require('./models/index')
 
 const app = express();
 app.use(cors());
 
-app.set('view engine', 'pug');
-app.use(logger('dev'));
+app.set("view engine", "pug");
+app.use(logger("dev"));
 app.use(bodyParser.json());
 
-app.use('/user', userRouter);
-app.use('/story', storyRouter);
+app.use("/user", userRouter);
+app.use("/story", storyRouter);
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 module.exports = app;
